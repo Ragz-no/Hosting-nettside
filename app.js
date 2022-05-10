ba0276b528fd63ffbf6eafb96a09f92494d77c0e
Ragz-no <70691805+Ragz-no@users.noreply.github.com>
2022-05-10 13:18:50 +0200

    added files

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const ejs = require("ejs");
 const app = express();
 const PORT = process.env.PORT || 6969; // for å få localhost til å kjøre med portnr
 
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) =>{
-    res.render("home");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/github", (req, res) => {
+  res.render("github");
+});
+
+app.get("/heroku", (req, res) => {
+  res.render("heroku");
 });
 
 app.listen(PORT, () => {
-    console.log("Server started on port http://localhost:6969/");
+  console.log("Server started on port http://localhost:6969/");
 });
