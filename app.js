89a80ec99f5293e7539734af03d1e10d69d721c8
Ragz-no <70691805+Ragz-no@users.noreply.github.com>
2022-05-10 12:52:24 +0200

    modified app.js

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 const express = require("express");
 const ejs = require("ejs");
 
+const app = express();
+const PORT = process.env.PORT || 6969; // for å få localhost til å kjøre med portnr
+
 
 app.get("/", (req, res) =>{
     res.render("home");
-  });
+});
+
+app.listen(PORT, () => {
+    console.log("Server started on port http://localhost:6969/");
+});
